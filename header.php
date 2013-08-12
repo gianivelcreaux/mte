9397ebf63afc9ce367621e23fc65c52048ff96bb
Sean Mooney <bookwerm@gmail.com>
2013-08-11 23:32:17 -0700

    Home Page Column Elements

--- header.php
+++ header.php
@@ -23,6 +23,7 @@
 	<meta name="viewport" content="width=device-width">
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
 	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic' rel='stylesheet' type='text/css'>
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<!--[if lt IE 9]>
 	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
--- index.php
+++ index.php
@@ -19,25 +19,30 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 
 		<div id="slider">
-			<?php echo do_shortcode('[wooslider slide_page="homepage" slider_type="slides"]') ?>
+			<?php echo do_shortcode('[wooslider slide_page="home_page" slider_type="slides"]') ?>
 		</div>
 
 		<div id="content" class="site-content" role="main">
-		<?php if ( have_posts() ) : ?>
 
-			<?php /* The loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php get_template_part( 'content', get_post_format() ); ?>
-			<?php endwhile; ?>
+		<div class="column_house">
+			<div class="column col_1">
+				<h3>Sed Sagitis</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse iaculis consequat risus, sed hendrerit eros cursus ac. Donec sed imperdiet eros. Sed sed velit pulvinar, facilisis ante quis, varius sem. Integer lectus felis, auctor non volutpat vel, tempor eget lorem. </p>
+			</div>
 
-			<?php twentythirteen_paging_nav(); ?>
+			<div class="column col_2">
+				<h3>Cras Luctus</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse iaculis consequat risus, sed hendrerit eros cursus ac. Donec sed imperdiet eros. Sed sed velit pulvinar, facilisis ante quis, varius sem. Integer lectus felis, auctor non volutpat vel, tempor eget lorem. </p>
+			</div>
 
-		<?php else : ?>
-			<?php get_template_part( 'content', 'none' ); ?>
-		<?php endif; ?>
-
+			<div class="column col_3">
+				<h3>Donec Vestibulum</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse iaculis consequat risus, sed hendrerit eros cursus ac. Donec sed imperdiet eros. Sed sed velit pulvinar, facilisis ante quis, varius sem. Integer lectus felis, auctor non volutpat vel, tempor eget lorem. </p>
+			</div>
+			</div>
 		</div><!-- #content -->
+		
 	</div><!-- #primary -->
-
 <?php get_sidebar(); ?>
+
 <?php get_footer(); ?>
